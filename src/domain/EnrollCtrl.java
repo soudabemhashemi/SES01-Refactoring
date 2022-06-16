514b3ee96f799278fecf04dbab872abf69e168c7
Sharif <s.mhashemi.221@gmail.com>
2022-06-16 12:19:51 +0430

    feat(Student): Add new method to calculate gpa

--- src/domain/EnrollCtrl.java
+++ src/domain/EnrollCtrl.java
@@ -39,19 +39,10 @@ public class EnrollCtrl {
     }
 
     public void checkGPALimit(List<CSE> courses, Student s) throws EnrollmentRulesViolationException {
-        Map<Term, Map<Course, Double>> transcript = s.getTranscript();
         int unitsRequested = 0;
         for (CSE o : courses)
             unitsRequested += o.getCourse().getUnits();
-        double points = 0;
-        int totalUnits = 0;
-        for (Map.Entry<Term, Map<Course, Double>> tr : transcript.entrySet()) {
-            for (Map.Entry<Course, Double> r : tr.getValue().entrySet()) {
-                points += r.getValue() * r.getKey().getUnits();
-                totalUnits += r.getKey().getUnits();
-            }
-        }
-        double gpa = points / totalUnits;
+        double gpa = s.getGPA();
         if ((gpa < 12 && unitsRequested > 14) ||
                 (gpa < 16 && unitsRequested > 16) ||
                 (unitsRequested > 20))
